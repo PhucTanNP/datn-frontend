64513303dbfe03a02300b6474c2ffed0d95680d9
PhucTanNP <phuctan977@gmail.com>
2026-04-20 00:01:45 +0700

    update sitemap

---
+++ sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://minhphatdrc.id.vn/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://minhphatdrc.id.vn/products',
+      lastModified: new Date(),
+    },
+  ];
+}
